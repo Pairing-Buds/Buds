802b0742b8fd82504713f6ebc78c3075f22390c0
SpecialSHipDobby <dkdi4224@naver.com>
2025-04-21 16:29:59 +0900

    Refactor: CsAnswer -> Answer 수정

---
+++ src/main/java/com/pairing/buds/domain/cs/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.pairing.buds.domain.cs.repository;
+
+import com.pairing.buds.domain.cs.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/main/java/com/pairing/buds/domain/cs/repository/CSRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pairing.buds.domain.cs.repository;
-
-import com.pairing.buds.domain.cs.entity.CSAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CSRepository extends JpaRepository<CSAnswer, Integer> {
-}
