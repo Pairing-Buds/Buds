d1ec9daea80095f52dcb7580b26dc720a9fbb54e
이윤호 <dldbsgh00005@gmail.com>
2025-04-22 11:28:47 +0900

    Feat: WakeTimeReqDto 추가
    - WakeTimeReqDto 추가

--- src/main/java/com/pairing/buds/domain/activity/controller/ActivityController.java
+++ src/main/java/com/pairing/buds/domain/activity/controller/ActivityController.java
@@ -1,9 +1,34 @@
 package com.pairing.buds.domain.activity.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/activity")
+@RequiredArgsConstructor
 public class ActivityController {
+
+    /** 기상 시간 등록 **/
+//    @PostMapping("/wake")
+//    public ResponseEntity<?> createWakeTime(){
+//
+//    }
+
+    /** 기상 시간 인증 **/
+
+    /** 기상 시간 수정 **/
+
+    /** 기상 시간 삭제 **/
+
+    /** 취침 시간 등록 **/
+
+    /** 취침 시간 인증 **/
+
+    /** 취침 시간 수정 **/
+
+    /** 취침 시간 삭제 **/
 }
---
+++ src/main/java/com/pairing/buds/domain/activity/dto/request/WakeTimeReqDto.java
@@ -0,0 +1,27 @@
+package com.pairing.buds.domain.activity.dto.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+@Valid
+public class WakeTimeReqDto {
+
+    @Positive(message = "Gotcha! Ha Ha!")
+    @NotNull(message = "Seriously? Please Give Me Right Data.. Not Abusing")
+    private int userId;
+
+    @NotEmpty
+    @NotBlank
+    @Length(min = 4, max = 4 , message = "Oh, Not 4 characters.. what's wrong with you?")
+    private String wakeTime;
+
+    @NotEmpty
+    @NotBlank
+    @Length(min = 4, max = 4 , message = "Oh, Not 4 characters.. what's wrong with you?")
+    private String sleepTime;
+}
