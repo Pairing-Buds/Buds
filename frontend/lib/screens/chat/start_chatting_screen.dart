d7d200b5b4a94f7f6aee0aeccc03f77fbab7b746
이채원 <lcw2094@gmail.com>
2025-04-25 07:46:27 +0900

    Feat: 채팅 시작 UI

---
+++ frontend/lib/screens/chat/start_chatting_screen.dart
@@ -0,0 +1,85 @@
+import 'package:buds/config/theme.dart';
+import 'package:flutter/material.dart';
+import 'voice_chatting_screen.dart';
+import 'chat_detail_screen.dart';
+
+class StartChattingScreen extends StatefulWidget {
+  const StartChattingScreen({super.key});
+
+  @override
+  State<StartChattingScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartChattingScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  void _handleSend() {
+    if (_controller.text.isNotEmpty) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ChatDetailScreen(message: _controller.text),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.cardBackground,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Opacity(opacity: 0.5,
+              child: Image.asset('assets/images/marmet_head.png', width: 240,)),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40, 0, 40, 40),
+            child: TextField(
+              controller: _controller,
+              style: const TextStyle(
+                color: Colors.black,
+              ),
+              decoration: InputDecoration(
+                hintText: '답장하기',
+                hintStyle: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                ),
+                filled: true,
+                fillColor: const Color(0xFFF5F5F5),
+                contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const VoiceChattingScreen(),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 12),
+                    child: Image.asset(
+                      'assets/icons/chat.png',
+                      width: 30,
+                    ),
+                  ),
+                ),
+                suffixIconConstraints: const BoxConstraints(
+                  minWidth: 40,
+                  minHeight: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(24),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
