b7d40dab049370ca6b2706ce6b31882a8dfe2b72
SpecialSHipDobby <dkdi4224@naver.com>
2025-05-01 13:48:59 +0900

    Refactor: 회원 관련 로직 수정 - 닉네임 랜덤 생성시 중복 확인 후 값 return - AuthService 주석 추가 - User entity에 페르소나 점수 여섯 가지 추가

--- backend/src/main/java/com/pairing/buds/domain/user/entity/User.java
+++ backend/src/main/java/com/pairing/buds/domain/user/entity/User.java
@@ -40,12 +40,38 @@ public class User extends CUBaseTime {
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
+    @Column(name = "letter_cnt", nullable = false)
+    private Integer letterCnt;
+
     @Column(name = "user_name", nullable = false)
     private String userName;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "user_character", nullable = false)
     private UserCharacter userCharacter;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "is_completed", nullable = false)
+    private SignupStatus isCompleted;
+
+    // 페르소나 점수 컬럼들
+    @Column(name = "seclusion_score", nullable = false)
+    private Integer seclusionScore;
+
+    @Column(name = "openness_score", nullable = false)
+    private Integer opennessScore;
+
+    @Column(name = "sociability_score", nullable = false)
+    private Integer sociabilityScore;
+
+    @Column(name = "routine_score", nullable = false)
+    private Integer routineScore;
+
+    @Column(name = "quietness_score", nullable = false)
+    private Integer quietnessScore;
+
+    @Column(name = "expression_score", nullable = false)
+    private Integer expressionScore;
 
     @OneToMany(
             mappedBy = "user",
@@ -55,13 +81,6 @@ public class User extends CUBaseTime {
     )
     private Set<Tag> tags = new HashSet<>();
 
-    @Column(name = "letter_cnt", nullable = false)
-    private Integer letterCnt;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "is_completed", nullable = false)
-    private SignupStatus isCompleted;
-
     @PrePersist
     private void prePersist() {
         if (this.isActive == null)    this.isActive = true;
@@ -69,6 +88,12 @@ public class User extends CUBaseTime {
         if (this.userCharacter == null) this.userCharacter = UserCharacter.GECKO;
         if (this.letterCnt == null)      this.letterCnt = 0;
         if (this.isCompleted == null)   this.isCompleted = SignupStatus.PENDING;
+        if (this.seclusionScore == null)          this.seclusionScore = 0;
+        if (this.opennessScore == null)       this.opennessScore = 0;
+        if (this.sociabilityScore == null)    this.sociabilityScore = 0;
+        if (this.routineScore == null)        this.routineScore = 0;
+        if (this.quietnessScore == null)      this.quietnessScore = 0;
+        if (this.expressionScore == null)     this.expressionScore = 0;
     }
 
 }
