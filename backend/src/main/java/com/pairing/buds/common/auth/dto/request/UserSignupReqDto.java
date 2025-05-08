31743a8cb47c4980f0a1ce2c82a9235ceeedf1df
이윤호 <dldbsgh00005@gmail.com>
2025-05-09 01:35:40 +0900

    Refactor:  ã태ê·ãEnum 클래스 수정 ë등

--- backend/src/main/java/com/pairing/buds/common/auth/dto/request/UserSignupReqDto.java
+++ backend/src/main/java/com/pairing/buds/common/auth/dto/request/UserSignupReqDto.java
@@ -4,6 +4,7 @@ import com.pairing.buds.domain.user.entity.User;
 import com.pairing.buds.domain.user.entity.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
@@ -17,11 +18,12 @@ import java.time.LocalDate;
 public class UserSignupReqDto {
 
     @NotBlank
-    @Email
+    @Email(regexp = "[0-9a-zA-Z]*@[a-z]*.[a-z]{2,3}]")
     private String userEmail;
 
     @NotBlank
     @Length(max = 50, message = "비밀번호는 최대 50자까지 지정 가능합니다.")
+    @Pattern(regexp = "[0-9a-zA-Z!@#(),.]{4,15}")
     private String password;
 
     private LocalDate birthDate;
