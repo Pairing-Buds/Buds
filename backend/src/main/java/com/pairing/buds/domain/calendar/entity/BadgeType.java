d2a7a3f8618cafbb7065bc95de8954fe412c2b7d
이윤호 <dldbsgh00005@gmail.com>
2025-05-10 12:22:15 +0900

    Refactor: 만보기 개수 추가 - 1000, 3000, 5000, 10000 - ㅂ만로보기 인증 시 리ì배지 종류 변경

--- backend/src/main/java/com/pairing/buds/domain/calendar/entity/BadgeType.java
+++ backend/src/main/java/com/pairing/buds/domain/calendar/entity/BadgeType.java
@@ -5,8 +5,11 @@ public enum BadgeType {
 
     /** 활동 뱃지 **/
     WAKE, // 기상 ( 미라클 모닝 )
-    WALK, // 만보기
-    VOICE_TEXT, // STT 변환된 명언 텍스트 
+    WALK1000, // 만보기 1000, 3000, 5000, 10000
+    WALK3000,
+    WALK5000,
+    WALK10000,
+    VOICE_TEXT, // STT 변환된 명언 텍스트
     TEXT, // 필사
     VISIT_PLACE, // 추천 장소 방문
 
